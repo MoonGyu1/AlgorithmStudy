21c9b4b3cf96571440735d57524adc57b572d571
gyuwonMoon <s201801402@hufs-gsuite.kr>
2024-08-12 20:11:24 +0900

    백준 - 1935. 후위 표기식2

---
+++ src/b01_data_structure/Solution1935.java
@@ -0,0 +1,53 @@
+// 후위 표기식2
+
+package src.b01_data_structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Solution1935 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Stack<Double> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(Character.isAlphabetic(c)){
+                int idx = Character.getNumericValue(c) - Character.getNumericValue('A');
+                st.push((double) arr[idx]);
+                continue;
+            }
+
+            Double op2 = st.pop();
+            Double op1 = st.pop();
+
+            switch (c){
+                case '+':
+                    st.push(op1 + op2);
+                    break;
+                case '-':
+                    st.push(op1 - op2);
+                    break;
+                case '*':
+                    st.push(op1 * op2);
+                    break;
+                case '/':
+                    st.push(op1 / op2);
+                    break;
+            }
+        }
+
+        System.out.printf("%.2f", st.pop());
+    }
+}
