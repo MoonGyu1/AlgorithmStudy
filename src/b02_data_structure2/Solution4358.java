c3bcb2584b384475213108550e93297ec4fcfb62
gyuwonMoon <s201801402@hufs-gsuite.kr>
2024-08-13 16:18:36 +0900

    백준 4358. 생태학

---
+++ src/b02_data_structure2/Solution4358.java
@@ -0,0 +1,34 @@
+// 생태학 (실버2)
+
+package src.b02_data_structure2;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+// 시간복잡도: O(n)
+public class Solution4358 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        HashMap<String, Integer> hm = new HashMap<>();
+        TreeSet<String> ts = new TreeSet<>();
+        int total = 0;
+
+        String name = "";
+        while((name = br.readLine()) != null && !name.isEmpty()) {
+            hm.put(name, hm.getOrDefault(name, 0) + 1);
+            ts.add(name);
+            total++;
+        }
+
+        for(String s: ts){
+            double rate = (double) hm.get(s) / total * 100;
+            bw.write(String.format("%s %.4f\n", s, rate));
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
