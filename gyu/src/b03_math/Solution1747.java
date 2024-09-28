04d318ab84ffcbfabb7d71e7a629ccec8b3eaa71
gyuwonMoon <s201801402@hufs-gsuite.kr>
2024-09-28 18:27:31 +0900

    백준 1747. 소수&팰린드롬

---
+++ gyu/src/b03_math/Solution1747.java
@@ -0,0 +1,41 @@
+// 소수&팰린드롬 (실버1)
+package src.b03_math;
+
+import java.util.Scanner;
+
+// 시간복잡도: O(n)
+public class Solution1747 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        while(true) {
+            if(isPalindrome(Integer.toString(n)) && isPrime(n)) {
+                System.out.println(n);
+                return;
+            }
+            n++;
+        }
+    }
+
+    static boolean isPrime(int n) {
+        if(n == 1) return false;
+
+        for(int i = 2; i <= n/2; i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static boolean isPalindrome(String n) {
+        for(int i = 0; i < n.length() / 2; i++) {
+            if(n.charAt(i) != n.charAt(n.length() - i - 1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
