b125645db6ded8c90a4e0b2c8dc9e79009cc3e9b
gyuwonMoon <s201801402@hufs-gsuite.kr>
2024-10-03 14:50:34 +0900

    add time complexity

--- gyu/src/b07_two_pointer/Solution20922.java
+++ gyu/src/b07_two_pointer/Solution20922.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
+// 시간복잡도: O(n)
 public class Solution20922 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- gyu/src/b07_two_pointer/Solution3151.java
+++ gyu/src/b07_two_pointer/Solution3151.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// 시간복잡도: O(n^2)
 public class Solution3151 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
