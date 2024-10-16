38b3d697753bb38d6961931033f3f4ba76e191ac
GimmeSpoon <skcjfdnd1996@naver.com>
2024-10-16 09:09:58 +0000

    update: cheolwoong 03math 22943

--- GimmeSpoon/b03_math/bj_1747.py
+++ GimmeSpoon/b03_math/bj_1747.py
@@ -3,8 +3,8 @@ import sys, math, queue
 def primetest(n):
     for p in range(2, math.floor(math.sqrt(n) + 1)):
         if n % p == 0:
-            return True
-    return False
+            return False
+    return True
 
 def digits_to_number(dg):
     n = 0
@@ -12,41 +12,51 @@ def digits_to_number(dg):
         n = n * 10 + d
     return n
 
-def d_palindromes(digit):
+def every_palindrome(digit):
 
-    q1, q2 = [0], []
+    palindromes = [0]
 
-    for _ in range(digit):
+    for d in range((digit - 1) % 2, digit, 2):
+        new_palindromes = []
 
-        while len(q1):
-            q = q1.pop()
-            for d in range(0, 10):
-                q2.append(q * 10 + d)
-                
-        temp = q1
-        q1 = q2
-        q2 = temp
+        for v in range(10):
+            for p in palindromes:
+                if d == 0:
+                    new_palindromes.append(p * 10 + (10 ** d) * v)
+                else:
+                    new_palindromes.append(p * 10 + (10 ** d) * v + v)
 
-    if len(q1):
-        return q1
-    else:
-        return q2
+        palindromes = new_palindromes
 
-def main (n):
+    return palindromes
+
+def get_digit(n):
+    d = 0
+    while n != 0:
+        n = n // 10
+        d += 1
+    return d
+
+def least_prime_palindrome (n):
     
-    exp = 0
+    exp = get_digit(n)
 
+    if n <= 2:
+        print(2)
+        return
+    
     while True:
-        
         for odd in (1, 3, 5, 7, 9):
-            base = odd * (10 ** exp) + odd
-            for palindrome in d_palindromes(exp - 1):
-                p = base + palindrome * 10 ** (exp - 1)
-                if p > n and primetest(palindrome):
+            base = odd * (10 ** (exp - 1))
+            if exp != 1:
+                base += odd
+            for palindrome in every_palindrome(exp - 2):
+                p = base + palindrome * 10
+                if p >= n and primetest(p):
                     print(p)
                     return
         exp += 1
 
 N = int(sys.stdin.readline())
 
-main(N)
+least_prime_palindrome(N)
---
+++ GimmeSpoon/b03_math/bj_22943.py
@@ -0,0 +1,6 @@
+import sys
+
+readline = sys.stdin.readline
+
+K, M = list(map(int, readline().split()))
+
