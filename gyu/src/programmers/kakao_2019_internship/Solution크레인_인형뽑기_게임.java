074bf7c8c5eb11b3c17fc6858c9a49698725e276
gyuwonMoon <s201801402@hufs-gsuite.kr>
2024-12-17 19:30:12 +0900

    프로그래머스 level1. 크레인 인형뽑기 게임

---
+++ gyu/src/programmers/kakao_2019_internship/Solution크레인_인형뽑기_게임.java
@@ -0,0 +1,44 @@
+package src.programmers.kakao_2019_internship;
+
+import java.util.*;
+
+// 시간복잡도: O(N^2 + M)
+public class Solution크레인_인형뽑기_게임 {
+
+	public int solution(int[][] board, int[] moves) {
+		int N = board.length;
+
+		ArrayList<Stack<Integer>> col = new ArrayList<>();
+		for (int i = 1; i <= N; i++) {
+			col.add(new Stack<>());
+		}
+
+		for (int j = 0; j < N; j++) {
+			for (int i = N - 1; i >= 0; i--) {
+				if (board[i][j] != 0) {
+					col.get(j+1).push(board[i][j]);
+				}
+			}
+		}
+
+		Stack<Integer> bucket = new Stack<>();
+		int cnt = 0;
+
+		for (int m : moves) {
+			if (col.get(m).isEmpty()) {
+				continue;
+			}
+
+			int current = col.get(m).pop();
+
+			if (!bucket.isEmpty() && bucket.peek() == current) {
+				bucket.pop();
+				cnt += 2;
+			} else {
+				bucket.push(current);
+			}
+		}
+
+		return cnt;
+	}
+}
