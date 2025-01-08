b33e1276fa92d31b314cd12be6a844a370601f90
gyuwonMoon <s201801402@hufs-gsuite.kr>
2025-01-08 16:22:50 +0900

    코딩테스트 책 - 01. 배열 정렬하기

---
+++ gyu/src/baekjoon/codingtest_java/Solution01.java
@@ -0,0 +1,24 @@
+package src.baekjoon.codingtest_java;
+
+import java.util.Arrays;
+
+// 시간복잡도: O(NlogN)
+public class Solution01 {
+	public static void main(String[] args) {
+		int[] org = {4, 2, 3, 1, 5};
+		int[] sorted = solution2(org);
+		System.out.println(Arrays.toString(org));
+		System.out.println(Arrays.toString(sorted));
+	}
+
+	private static int[] solution(int[] arr) {
+		Arrays.sort(arr); // 원본 배열 자체를 정렬
+		return arr;
+	}
+
+	private static int[] solution2(int[] arr) {
+		int[] clone = arr.clone(); // 원본 배열 상태 유지해야하는 경우 clone() 메서드 사용
+		Arrays.sort(clone);
+		return clone;
+	}
+}
