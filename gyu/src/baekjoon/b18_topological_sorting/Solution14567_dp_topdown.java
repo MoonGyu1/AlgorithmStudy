1e01cbf3dca66da3094f13e8c847ebe616daeded
gyuwonMoon <s201801402@hufs-gsuite.kr>
2025-02-28 17:39:23 +0900

    백준 14567. 선수과목 - Top-Down DP

---
+++ gyu/src/baekjoon/b18_topological_sorting/Solution14567_dp_topdown.java
@@ -0,0 +1,75 @@
+// 선수과목 (Prerequisite)
+
+package src.baekjoon.b18_topological_sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+ * DP Top-Down 방식
+ */
+// 시간복잡도: O(N+M)
+public class Solution14567_dp_topdown {
+
+	static int N, M;
+	static ArrayList<Integer>[] prereq;
+	static int[] minSem;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		prereq = new ArrayList[N+1];
+		for(int i = 1; i <= N; i++) {
+			prereq[i] = new ArrayList<>();
+		}
+
+		minSem = new int[N+1];
+
+		while(M --> 0) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+
+			prereq[B].add(A);
+		}
+
+		for(int i = 1; i <= N; i++) {
+			minSem[i] = dp(i);
+		}
+
+		for(int i = 1; i <= N; i++) {
+			System.out.printf("%d ", minSem[i]);
+		}
+	}
+
+	static int dp(int s) {
+		if(minSem[s] != 0) return minSem[s]; // 이미 해를 구한 경우
+		if(prereq[s].isEmpty()) return 1; // Base case
+
+		int max = 1;
+		for(int pre : prereq[s]) {
+			max = Math.max(max, dp(pre));
+		}
+
+		minSem[s] = max + 1;
+
+		return minSem[s];
+	}
+}
+
+/**
+ * 1. DFS로, 선수과목(루트)부터 완전탐색하면 안 되는 이유
+ * => 마치 피보나치 수열을 재귀적으로 계산할 때처럼 시간복잡도가 O(N!)이 되기 때문
+ *
+ * 2. DFS에서 visited 배열로 노드당 1회 방문으로 제한할 수 없는 이유
+ * => 어떤 노드를 먼저 방문했는 지에 따라, 거리가 달라지기 때문
+ *
+ * DFS로 선수 과목부터 탐색할 수는 없고, Top-Down DP처럼 노드당 1회만 계산하기
+ */
