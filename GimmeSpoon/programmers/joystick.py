df3e5b9b543560b78c623c1b556875ee789580f1
GimmeSpoon <skcjfdnd1996@naver.com>
2025-04-12 18:09:21 +0900

    programmers joystick complete

--- GimmeSpoon/programmers/joystick.py
+++ GimmeSpoon/programmers/joystick.py
@@ -1,27 +1,27 @@
-import queue
-
 def solution(name):
-    answer = 0
-    indexes = [0]
-    for i, c in enumerate(name):
-        action = min(ord(c) - ord("A"), ord("Z") - ord(c) + 1)
-        answer += action
-        if i != 0 and action > 0:
-            indexes.append(i)
-            
-    max_dist = 0
-    max_pair = None
-    for i, idx in enumerate(indexes):        
-        d = (idx - indexes[i - 1]) % len(name)
-        if d > max_dist:
-            max_dist = d
-            max_pair = (indexes[i - 1], idx)
-            
-    if max_pair is not None:
-        i, j = min(max_pair), max(max_pair)
-        if i == 0 or j == 0:
-            answer += len(name) - max_dist
-        else:
-            answer += min(i, len(name) - j) * 2 + max(i, len(name) - j)
     
-    return answer
+    total_actions = 0
+    non_A_indexes = [0]
+    
+    for i, char in enumerate(name):
+        if char != "A":
+            char = ord(char)
+            total_actions += min(char - ord("A"), ord("Z") - char + 1)
+            if i > 0:
+                non_A_indexes.append(i)
+            
+    min_moves = len(name)
+    
+    for i, idx in enumerate(non_A_indexes):
+        if i < len(non_A_indexes) - 1:
+            moves = min(idx * 2 + len(name) - non_A_indexes[i + 1],
+                        idx + (len(name) - non_A_indexes[i + 1]) * 2)
+        elif i > 0:
+            moves = min(idx, len(name) - idx + non_A_indexes[i - 1] * 2)
+        else:
+            moves = 0
+            
+        if moves < min_moves:
+            min_moves = moves
+            
+    return total_actions + min_moves
